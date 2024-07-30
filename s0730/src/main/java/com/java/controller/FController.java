793667070389a93f3767706a40d19db796b51469
lalabears <132989641+lalabears@users.noreply.github.com>
2024-07-30 10:31:04 +0900

    -

--- s0730/src/main/java/com/java/controller/FController.java
+++ s0730/src/main/java/com/java/controller/FController.java
@@ -1,7 +1,15 @@
 package com.java.controller;
 
+import java.io.File;
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.java.dto.Board;
 
 @Controller
 public class FController {
@@ -9,4 +17,35 @@ public class FController {
 	public String index() {
 		return "index";
 	}
+	@RequestMapping("/form1")
+	public String form1() {
+		return "form1";
+	}
+	@RequestMapping("/form2")
+	public String form2() {
+		return "form2";
+	}
+	@RequestMapping("/doForm1")
+	public String doForm1(Board board,
+			@RequestPart MultipartFile file, Model model) throws Exception {
+		String fileName="";   // 파일명
+		if(!file.isEmpty()) {   // 파일을 upload에 저장 
+			String ori_fileName = file.getOriginalFilename();
+			UUID uuid = UUID.randomUUID();
+			fileName = uuid+"_"+ori_fileName;
+			String uploadUrl = "c:/upload/";
+			File f = new File(uploadUrl+fileName);
+			file.transferTo(f);
+		}
+		model.addAttribute("fileName", fileName);
+		model.addAttribute("btitle", board.getBtitle());
+		return "fileCheck";
+	}
+	
+	@RequestMapping("/doForm2")
+	public String doForm2() {
+		return "fileCheck2";
+	}
+	
+	
 }
--- s0730/src/main/webapp/WEB-INF/views/index.jsp
+++ s0730/src/main/webapp/WEB-INF/views/index.jsp
@@ -2,11 +2,15 @@
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>index.jsp</title>
+	</head>
+	<body>
+		<h1>main page</h1>
+		<ul>
+			<li><a href="form1">form1</a></li>
+			<li><a href="form2">form2</a></li>
+		</ul>
+	</body>
 </html>
