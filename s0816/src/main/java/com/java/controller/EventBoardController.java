cec0bdf69122f836923815982d2af374ae148667
lalabears <132989641+lalabears@users.noreply.github.com>
2024-08-16 12:12:55 +0900

    -

--- s0816/src/main/java/com/java/controller/EventBoardController.java
+++ s0816/src/main/java/com/java/controller/EventBoardController.java
@@ -1,10 +1,12 @@
 package com.java.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EventBoardController {
+	@Autowired 
 	@RequestMapping("/board/eventlist")
 	public String eventlist() {
 		return "board/eventlist";
---
+++ s0816/src/main/java/com/java/mapper/EventBoardMapper.java
@@ -0,0 +1,15 @@
+package com.java.mapper;
+
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.java.dto.Board;
+import com.java.dto.Page;
+
+@Mapper
+public interface EventBoardMapper {
+
+	
+
+}
